0f473c9638ebf0e37cb5c28fb7ff4227e10ede87
SimonSmeets <simon-smeets@hotmail.be>
2020-02-06 14:09:00 +0100

    Update finetuning_tools.py

--- finetuning_tools.py
+++ finetuning_tools.py
@@ -6,7 +6,7 @@ import numpy as np
 from skimage.transform import resize
 from dataset_tool import create_from_images
 import matplotlib
-
+import shutil
 def dataset_to_res(loc, output_loc,res):
     print(loc)
     try:
@@ -38,9 +38,34 @@ def dir_to_frames(input_loc, output_loc, res):
             matplotlib.pyplot.imsave(os.path.join(output_loc,imagename),image)
 
 
+def split_casia_finetuning(loc, output_loc):
+    print(loc)
+    try:
+        os.mkdir(os.path.join(os.path.join(output_loc,"real/")))
+        os.mkdir(os.path.join(os.path.join(output_loc,"attack/")))
+    except OSError:
+        pass
+    for path in os.listdir(loc):
+        if os.path.isdir(os.path.join(loc,path)):
+            try:
+                os.mkdir(os.path.join(os.path.join(output_loc,"real/"),os.path.split(path)[-1]))
+                os.mkdir(os.path.join(os.path.join(output_loc,"attack/"),os.path.split(path)[-1]))
+
+            except OSError:
+                pass
+            split_casia_finetuning(os.path.join(loc,path),output_loc)
+        else:
+            if path.startswith(("1","2","HR_1")):
+                shutil.copy2(os.path.join(loc,path),os.path.join(os.path.join(output_loc,"real/"),os.path.join(os.path.split(loc)[-1],path)))
+            else:
+                shutil.copy2(os.path.join(loc,path),os.path.join(os.path.join(output_loc,"attack/"),os.path.join(os.path.split(loc)[-1],path)))
+    print("dir done")
+
+
 res = 128
-path = "../databases/replay-attack/train_frames"
-#dataset_to_res(path,"../databases/replay-attack/train_frames_128",128)
-create_from_images("../databases/replay-attack/real_tfrecords","../databases/replay-attack/train_frames_128/real/",True)
+path = "../databases/casia-fasd/train_frames"
+#dataset_to_res(path,"../databases/casia-fasd/train_frames_128",128)
+#split_casia_finetuning("../databases/casia-fasd/train_frames_128/train_release", "../databases/casia-fasd/train_frames_128")
+create_from_images("../databases/casia-fasd/real_tfrecords","../databases/casia-fasd/train_frames_128/real/*",True)
 
 #test_images, test_labels = get_all_images(path, "attack")
